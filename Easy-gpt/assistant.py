47141b3f9df385a9b9f9e7c97a99bd8401c58b87
Adpros7 <pranit.advik@gmail.com>
2025-09-08 16:53:52 -0400

    implemented default convo and web search

--- Easy-gpt/assistant.py
+++ Easy-gpt/assistant.py
@@ -6,9 +6,9 @@ from openai import OpenAI
 from openai.types.shared_params import ResponsesModel, Reasoning
 from os import getenv
 from typing_extensions import TypedDict
-import random
 from openai.types.responses.response_conversation_param import ResponseConversationParam
 from openai.types.conversations.conversation import Conversation
+from openai.types.vector_store import VectorStore
 
 # e.g. {"type": "string"}
 PropertySpec: TypeAlias = dict[str, str]
@@ -40,8 +40,8 @@ class CustomToolInputFormat:
 
 
 class Assistant:
-    def __init__(self, api_key: str | None, model: ResponsesModel, system_prompt: str = "", temperature: float | None = None, reasoning_effort: Literal["minimal", "low", "medium", "high"] = "medium", summary_length: Literal["auto", "concise", "detailed"] = "auto"):
-        """Initialize the Assistant with configuration parameters. ONLY USE REASONING  WITH GPT-5 and o MODELS."""
+    def __init__(self, api_key: str | None, model: ResponsesModel, system_prompt: str = "", default_conversation: Conversation | bool = True, temperature: float | None = None, reasoning_effort: Literal["minimal", "low", "medium", "high"] = "medium", summary_length: Literal["auto", "concise", "detailed"] = "auto"):
+        """Initialize the Assistant with configuration parameters. ONLY USE REASONING  WITH GPT-5 and o MODELS. Temp only with bigger models"""
         self.model = model
         if not api_key:
             if not getenv("OPENAI_API_KEY"):
@@ -51,6 +51,7 @@ class Assistant:
                 self.api_key = getenv("OPENAI_API_KEY")
         else:
             self.api_key = api_key
+        self.client = OpenAI(api_key=self.api_key)
         self.system_prompt = system_prompt
         self.temperature = temperature
         self.reasoning_effort = reasoning_effort
@@ -63,33 +64,70 @@ class Assistant:
 
         else:
             self.temperature = temperature
+            
+        if default_conversation:
+            self.conversation = self.create_conversation()
+            self.conversation_id = self.conversation.id # type: ignore
+        else:
+            self.conversation = None
+            self.conversation_id = None
 
-        self.client = OpenAI(api_key=self.api_key)
+
+    def _convert_filepath_to_vector(self, filepath: str) -> VectorStore:
+        # Step 1: create the vector store
+        filename = os.path.basename(filepath)
+        vector_store = self.client.vector_stores.create(name=filename)
+        
+
+        # Step 2: upload the file into the vector store
+        with open(filepath, "rb") as f:
+            self.client.vector_stores.files.upload_and_poll(
+                vector_store_id=vector_store.id,
+                file=f
+            )
+
+        return vector_store
+
+    
 
     def chat(
         self,
         input: str,
-        conv_id: str | None | Conversation = None,
+        conv_id: str | None | Conversation | bool = True,
         max_output_tokens: int | None = None,
         store: bool | None = False,
-        reasoning: Reasoning | None = None,
+        web_search: bool | None = None,
+        code_interpreter: bool | None = None,
+        image_generator: bool | None = None,
+        file_search: list[str | bytes] | None = None,
         return_full_response: bool = False,
 
     ):
-        """Reasoning can only have gpt 5 and o and temp only to the big boy models"""
+        """Reasoning can only have gpt 5 and o and temp only to the big boy models set conv_id to True to use default conversation"""
         params = {
             "model": self.model,
             "input": input,
             "instructions": self.system_prompt if self.system_prompt else "",
             "temperature": self.temperature if self.temperature else None,
-            "reasoning": reasoning if reasoning else None,
             "max_output_tokens": max_output_tokens if max_output_tokens else None,
             "store": store if store else None,
-            "conversation": conv_id if conv_id else None,
-            "return_full_response": return_full_response
+            "conversation": conv_id if conv_id is str else None,
+            "return_full_response": return_full_response,
+            "tools": []
         }
-
-        clean_params = {k: v for k, v in params.items() if v is not None}
+        
+        if params["conversation"] is None:
+            if conv_id is True:
+                params["conversation"] = self.conversation_id
+            elif isinstance(conv_id, Conversation):
+                params["conversation"] = conv_id.id
+            else:
+                params["conversation"] = None
+                
+        if web_search:
+            params["tools"].append({"type": "web_search"})
+        
+        clean_params = {k: v for k, v in params.items() if v is not None or "" or [] or {}}
         clean_params.__delitem__("return_full_response")
         response = self.client.responses.create(
             **clean_params
@@ -98,7 +136,7 @@ class Assistant:
 
         if return_full_response:
             return [response, response.output_text]
-
+        
         else:
             return [response.output_text, response.conversation]
 
@@ -143,19 +181,11 @@ if __name__ == "__main__":
                     system_prompt="You are a helpful assistant.")
 
     # Create a conversation on the OpenAI server
-    conv_id = bob.create_conversation(return_id_only=True)
 
     while True:
         user_input = input("User: ")
         # Only send id if it's valid
-        if conv_id:
-            response = bob.chat(input=user_input, conv_id=conv_id, store=True)
-            conv_id = response[1]
-        else:
-            response = bob.chat(input=user_input, store=True,
-                                return_full_response=True)
-            print(response)
-            conv_id = response[1]
+        response = bob.chat(user_input, conv_id=True, web_search=True)
 
         print("Assistant:", response[0])
         print("Conv ID:", response[1])
