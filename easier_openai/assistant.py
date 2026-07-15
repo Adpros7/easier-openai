d6935a3d4894dd4e62d356d2ec35f2b0ba20e0de
Adpros7 <pranit.advik@gmail.com>
2026-07-15 10:50:55 +0530

    refactor(openai): update model handling and README typo

--- easier_openai/assistant.py
+++ easier_openai/assistant.py
@@ -1,7 +1,21 @@
+from httpx import URL
+from openai.types.conversations.conversation import Conversation
 from typing import Optional
 from openai import OpenAI
-from .models import Models
+from .models import Model
 
 class Assistant:
-    def __init__(self, api_key: Optional[str] = None, models: Models = "gpt-5.5"):
-        self.client = OpenAI()
+    def __init__(self, api_key: Optional[str] = None, model: Model = "gpt-5.5", base_url: str = "https://api.openai.com/v1", conversation: bool = True):
+        self.client = OpenAI(api_key=api_key, base_url=base_url)
+        self.conversation: Conversation | None = self.client.conversations.create() if conversation else None
+        self.model = model
+    
+    def change_model(self, model: Model):
+        self.model = model
+    
+    def new_conversation(self):
+        self.conversation = self.client.conversations.create()
+    
+    def change_base_url(self, new_url: str):
+        self.client._base_url = URL(new_url)
+    
--- easier_openai/make_model_list.py
+++ easier_openai/make_model_list.py
@@ -7,6 +7,6 @@ models = [i.id for i in OpenAI().models.list()]
 
 CODE = f"""from typing import TypeAlias, Literal
 
-Models: TypeAlias = Literal{models.__repr__()}"""
+Model: TypeAlias = Literal{models.__repr__()}"""
 
-Path("models.pyi").write_text(CODE)
+Path("models.py").write_text(CODE)
